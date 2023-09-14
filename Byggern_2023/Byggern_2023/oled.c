b84824e2c37d994dc277b28dae38de9524fdcd58
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2023-09-14 12:48:03 +0200

    added oled boilerplate code

---
+++ Byggern_2023/Byggern_2023/oled.c
@@ -0,0 +1,10 @@
+#include "oled.h"
+
+void oled_init() {}
+void oled_reset() {}
+void oled_home() {}
+void oled_goto_line(uint8_t line) {}
+void oled_goto_column(uint8_t column) {}
+void oled_clear_line(uint8_t line) {}
+void oled_pos(uint8_t row, uint8_t column) {}
+void oled_print(char* str) {}
---
+++ Byggern_2023/Byggern_2023/oled.h
@@ -0,0 +1,14 @@
+#ifndef OLED_H
+#define OLED_H
+
+
+void oled_init();
+void oled_reset();
+void oled_home();
+void oled_goto_line(uint8_t line);
+void oled_goto_column(uint8_t column);
+void oled_clear_line(uint8_t line);
+void oled_pos(uint8_t row, uint8_t column);
+void oled_print(char* str);
+
+#endif
